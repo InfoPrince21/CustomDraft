7dc12eb96f61408a457e1cc0d06141aec97664a9
Prince <princeitschool@gmail.com>
2022-06-20 13:47:45 -0500

    ContactPage

--- src/features/display/DisplayList.js
+++ src/features/display/DisplayList.js
@@ -1,18 +1,22 @@
 import { Col, Row } from 'reactstrap';
-import DisplayCard from './DisplayCard';
+// import DisplayCard from './DisplayCard';
+import AnimatedDisplayCard from './AnimatedDisplayCard';
 import { selectFeaturedCampsite } from '../campsites/campsitesSlice';
 import { selectFeaturedPromotion } from '../promotions/promotionsSlice';
+import { selectFeaturedPartners } from '../partners/partnersSlice.js';
 
 const DisplayList = () => {
-    const items = [selectFeaturedCampsite(), selectFeaturedPromotion()];
+    const items = [selectFeaturedCampsite(), selectFeaturedPromotion(), selectFeaturedPartners(),];
 
     return (
         <Row>
             {items.map((item, idx) => {
                 return (
-                    <Col md className='m-1' key={idx}>
-                        <DisplayCard item={item} />
-                    </Col>
+                    item && (
+                        <Col md className='m-1' key={idx}>
+                            <AnimatedDisplayCard item={item} />
+                        </Col>
+                    )
                 );
             })}
         </Row>
